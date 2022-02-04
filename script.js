b50ed99be0de50d05e46c8f9449bdd1732be5d09
Hemant Chaubey <codewithhemant@gmail.com>
2022-02-04 21:34:00 +0530

    added showTime functionand linked pages

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-var date = new Date();
-var h = date.getHours();
-var m = date.getMinutes();
-var s = date.getSeconds();
-console.log( h + ":" + m + ":" + s )
+function showTime(){
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  
+  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
