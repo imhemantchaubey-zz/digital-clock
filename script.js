9db79a38ebd4d1f5f1ae6bd7f9caaef0ef78aad7
Hemant Chaubey <codewithhemant@gmail.com>
2022-02-05 20:34:00 +0530

    solved the problem of one digit numeric

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function showTime(){
   var h = date.getHours();
   var m = date.getMinutes();
   var s = date.getSeconds();
+
+  h = (h<10) ? '0' + h : h;
+  m = (m<10) ? '0' + m : m;
+  s = (s<10) ? '0' + s : s;
   
   document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
 
